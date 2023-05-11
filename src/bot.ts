2c4693a6ebb1d55adbd802c8b1d1848bf8cac316
Jaiwanth <jaiwanth2011@gmail.com>
2023-05-11 18:48:44 +0530

    feat: update arguments

--- src/bot.ts
+++ src/bot.ts
@@ -10,8 +10,14 @@ import puppeteer from "puppeteer";
 let browser, page;
 
 (async () => {
-  browser = await puppeteer.launch({ headless: true });
+  browser = await puppeteer.launch({
+    userDataDir: "./user_data",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
   page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+  );  
   console.log("Browser started");
 })();
 
